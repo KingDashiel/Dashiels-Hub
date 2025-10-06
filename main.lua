5679984f210841692879ea5726d8404e6a044e72
KingDashiel <124647386+KingDashiel@users.noreply.github.com>
2025-10-06 03:59:57 +0000

    a

--- main.lua
+++ main.lua
@@ -1,1 +1,5 @@
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/deividcomsono/Obsidian/refs/heads/main/Library.lua/'))
+local Tabs = {
+    Main = Window:AddTab("Main", "user"),
+    ["UI Settings"] = Window:AddTab("UI Settings", "settings"),
+}
