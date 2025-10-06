0151cdf6649123e207ff5b31f34162f62f753b95
KingDashiel <124647386+KingDashiel@users.noreply.github.com>
2025-10-06 04:15:29 +0000

    a1

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deividcomsono/Obsidian/refs/heads/main/Library.lua"))()
+local Window = Library:CreateWindow({
+    Title = "mspaint",
+    Footer = "version: example",
+    Icon = 95816097006870,
+    NotifySide = "Right",
+})
 local Tabs = {
     Main = Window:AddTab("Main", "user"),
-    ["UI Settings"] = Window:AddTab("UI Settings", "settings"),
-}
-local Tabs = {
-    Main = Window:AddTab("asd", "ad"),
     ["UI Settings"] = Window:AddTab("UI Settings", "settings"),
 }
